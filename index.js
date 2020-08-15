f940c2962629fc607512e1ba9a8b7f978803f6d5
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-15 18:40:07 +0100

    add corrected feach postBeach

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const endPoint = "http://127.0.0.1:3000/api/v1/beaches"
 document.addEventListener("DOMContentLoaded", () => {
     getBeaches()
 
-    
+     let createBeachForm = document.querySelector('#create-beach-form')
+    //  createBeachForm.addEventListener('submit', (event) => console.log(event))
 
-    let createBeachForm = document.querySelector('#create-beach-form')
-
+    // after form is submited it's calling form handeler, event is when submit is is pressed, then I need to send back data to backend to database , to create new instance of beach, and I need post fetch for that
     createBeachForm.addEventListener('submit', (event) => createFormHandler(event))
-    createFormHandler(event);
+    // createFormHandler(event);
 })
 
 function getBeaches() {
@@ -19,61 +19,64 @@ function getBeaches() {
           // remember our JSON data is a bit nested due to our serializer
         beaches.data.forEach(beach => {
           // double check how your data is nested in the console so you can successfully access the attributes of each individual object
-          
           const beachMarkup = `
             <div data-id=${beach.id}>
               <h3>${beach.attributes.name}</h3>
               <p>${beach.attributes.country.name}</p>
               <p>${beach.attributes.location}</p>
-              <p>${beach.attributes.desription}</p>
+              <img src=${beach.attributes.image_url} height="200" width="250">
+              <br><br>
               <button data-id=${beach.id}>edit</button>
             </div>
             <br><br>`;
-
+  
             document.querySelector('#beach-container').innerHTML += beachMarkup
         })
-
     })
 }
 
-    function createFormHandler(event) {
-        event.preventDefault()
-        const nameInput = document.querySelector('#input-name').value
-        const countryId = parseInt(document.querySelector('#countries').value)
-        const locationInput = document.querySelector('#input-location').value
-        const descriptionInput = document.querySelector('#input-description').value
-        const imageInput = document.querySelector('#input-url').value
+
+
+function createFormHandler(event) {
+    event.preventDefault()
+    
+    const nameInput = document.querySelector('#input-name').value
+    const countryId = parseInt(document.querySelector('#countries').value)
+    const locationInput = document.querySelector('#input-location').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const imageInput = document.querySelector('#input-url').value
         
-        postBeach(nameInput, countryId, locationInput, descriptionInput, imageInput)
-    }
-    function postFetch(name, country_id, location, desription, image_url) {
-        console.log(name, country_id, location, desription, image_url);
-        let bodyData = {name, country_id, location, description, image_url}
+    postBeach(nameInput, countryId, locationInput, descriptionInput, imageInput)
+}
 
-        fetch(endPoint, {
-            // POST request
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(bodyData)
-        })
-        .then(response => response.json())
-        .then(beach => {
-            console.log(beach);
-            const beachData = beach.data.attributes
-            // render JSON response
-            const beachMarkup = `
-            <div data-id=${beach.id}>
-            <h3>${beachData.name}</h3>
-            <p>${beachData.country.name}</p>
-            <p>${beachData.location}</p>
-            <p>${beachData.description}</p>
-            <img src=${beachData.image_url} height="200" width="250">
-            <button data-id=${beachData.id}>edit</button>
-            </div>
-            <br><br>`;
+function postBeach(name, country_id, location, description, image_url) {
+    // console.log(name, country_id, location, description, image_url);
+    let bodyData = {name, country_id, location, description, image_url}
 
-            document.querySelector('#beach-container').innerHTML += beachMarkup;
-        }) 
-    }
+    fetch(endPoint, {
+        // POST request
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(beach => {
+        // console.log(beach);
+        const beachData = beach.data
+        // render JSON response, render data to user to see what created, manuplate DOM by showing user what created, data is pointing to single object not array like in get fetch where I had arrey and .forEach
+        const beachMarkup = `
+        <div data-id=${beach.id}>
+        <h3>${beachData.attributes.name}</h3>
+        <p>${beachData.attributes.country.name}</p>
+        <p>${beachData.attributes.location}</p>
+        <p>${beachData.attributes.description}</p>
+        <img src=${beachData.image_url} height="200" width="250">
+        <button data-id=${beachData.id}>edit</button>
+        </div>
+        <br><br>`;
+        // I am selecting container and updaing HTML
+        document.querySelector('#beach-container').innerHTML += beachMarkup;
+    }) 
+}
 
 
